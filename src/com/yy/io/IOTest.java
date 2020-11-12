3bfc471f595f7833eb67ec5506ef4fd3563a7cd5
yloopdaed <yykeepbreak@gmail.com>
2020-11-12 16:43:21 +0800

    20201112 bio/nio/epoll

--- src/com/yy/io/IOTest.java
+++ src/com/yy/io/IOTest.java
@@ -7,53 +7,35 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-
 public class IOTest {
 
-//    public static void main(String[] args) throws IOException {
-//        ServerSocketChannel ss = ServerSocketChannel.open();
-//        Selector selector = Selector.open(); // 优先选择epoll
-//
-//
-//        BitMap bm = new BitMap(16);
-//        bm.atPut(0,true);
-//
-//        System.out.println(bm.at(1));
-//
-//
-//    }
 
     public static void main(String[] args) throws Exception {
 
+        // 服务端建立连接
         ServerSocket serverSocket = new ServerSocket(8000);
 
-        // (1) 接收新连接线程
-        new Thread(() -> {
-            while (true) {
-                try {
-                    // (1) 阻塞方法获取新的连接
-                    Socket socket = serverSocket.accept();
-                    // (2) 每一个新的连接都创建一个线程，负责读取数据
-                    new Thread(() -> {
-                        try {
-                            int len;
-                            byte[] data = new byte[1024];
-                            InputStream inputStream = socket.getInputStream();
-                            // (3) 按字节流方式读取数据
-                            while ((len = inputStream.read(data)) != -1) {
-                                System.out.println(new String(data, 0, len));
-                            }
-                        } catch (IOException e) {
+        while (true) {
+            try {
+                // 1 获取新的连接, 阻塞
+                Socket socket = serverSocket.accept();
+                // 2 每一个新的连接都创建一个线程，负责读取数据
+                new Thread(() -> {
+                    try {
+                        int len;
+                        byte[] data = new byte[1024];
+                        InputStream inputStream = socket.getInputStream();
+                        // 3 按字节流方式读取数据
+                        while ((len = inputStream.read(data)) != -1) {
+                            System.out.println(new String(data, 0, len));
                         }
-                    }).start();
+                    } catch (IOException e) {
+                    }
+                }).start();
 
-                } catch (IOException e) {
-                }
+            } catch (IOException e) {
             }
-        }).start();
+        }
     }
 
 
--- src/com/yy/io/NIOTest.java
+++ src/com/yy/io/NIOTest.java
@@ -3,92 +3,44 @@ package com.yy.io;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.LinkedList;
 
 public class NIOTest {
 
+    public static void main(String[] args) throws IOException {
 
-        /**
-         * serverSelector负责轮询是否有新的连接,clientSelector负责轮询连接是否有数据可读.
-         * 服务端监测到新的连接不再创建一个新的线程,而是直接将新连接绑定到clientSelector上,这样不用IO模型中1w个while循环在死等
-         * clientSelector被一个while死循环包裹,如果在某一时刻有多条连接有数据可读通过 clientSelector.select(1)方法轮询出来进而批量处理
-         * 数据的读写以内存块为单位
-         *
-         * @param args
-         * @throws IOException
-         */
-        public static void main(String[] args) throws IOException {
-            Selector serverSelector = Selector.open();
-            Selector clientSelector = Selector.open();
+        LinkedList<SocketChannel> clients = new LinkedList<>();
 
-            new Thread(() -> {
-                try {
-                    ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-                    serverSocketChannel.socket().bind(new InetSocketAddress(8000));
-                    serverSocketChannel.configureBlocking(false); // 配置阻塞
-                    serverSocketChannel.register(serverSelector, SelectionKey.OP_ACCEPT);
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open(); // 服务端建立连接
+        serverSocketChannel.bind(new InetSocketAddress(8000));
+        serverSocketChannel.configureBlocking(false); // 非阻塞
 
-                    while (true) {
-                        // 轮询监测是否有新的连接
-                        if (serverSelector.select(1) > 0) {
-                            Set<SelectionKey> selectionKeys = serverSelector.selectedKeys();
-                            Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
-                            while (keyIterator.hasNext()) {
-                                SelectionKey selectionKey = keyIterator.next();
-                                if (selectionKey.isAcceptable()) {
-                                    try {
-                                        //(1)每来一个新连接不需要创建一个线程而是直接注册到clientSelector
-                                        SocketChannel socketChannel = ((ServerSocketChannel) selectionKey.channel()).accept();
-                                        socketChannel.configureBlocking(false);
-                                        socketChannel.register(clientSelector, SelectionKey.OP_READ);
-                                    } finally {
-                                        keyIterator.remove();
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+        while (true) {
+            SocketChannel clientSocketChannel = serverSocketChannel.accept(); // socket客户端连接
+
+            if (clientSocketChannel != null) {
+                clientSocketChannel.configureBlocking(false);
+                clients.add(clientSocketChannel);
+                System.out.println(clientSocketChannel.socket().getPort());
+            }
+
+            // 遍历已连接的客户端socket
+            for (SocketChannel client: clients) { // 这里涉及到用户态和内核态的切换
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                int num = client.read(buffer);
+                if (num > 0) {
+                    buffer.flip();
+                    byte[] bytes = new byte[buffer.limit()];
+                    buffer.get(bytes);
+                    System.out.println(new String(bytes));
                 }
-            }).start();
-
-            new Thread(() -> {
-                try {
-                    while (true) {
-                        // (2)批量轮询是否有哪些连接有数据可读
-                        if (clientSelector.select(1) > 0) {
-                            Set<SelectionKey> selectionKeys = serverSelector.selectedKeys();
-                            Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
-                            while (keyIterator.hasNext()) {
-                                SelectionKey selectionKey = keyIterator.next();
-                                if (selectionKey.isReadable()) {
-                                    try {
-                                        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-                                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                                        //(3)读取数据以块为单位批量读取
-                                        socketChannel.read(byteBuffer);
-                                        byteBuffer.flip();
-                                        System.out.println(Charset.defaultCharset().newDecoder().decode(byteBuffer)
-                                                .toString());
-                                    } finally {
-                                        keyIterator.remove();
-                                        selectionKey.interestOps(SelectionKey.OP_READ);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }).start();
+            }
         }
+
+
+    }
+
 
 }
