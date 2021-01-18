9d856749c841588985a24445dc1ade64e983fd43
yloopdaed-hme <521300259@qq.com>
2021-01-19 00:35:46 +0800

    20210119 customthreadpool

--- src/main/java/com/yy/thread/ExecutorsTest.java
+++ src/main/java/com/yy/thread/ExecutorsTest.java
@@ -1,15 +1,39 @@
 package com.yy.thread;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.yy.thread.pool.ThreadPoolTools;
 import org.quartz.simpl.SimpleThreadPool;
 
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExecutorsTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
-                .setNameFormat("demo-pool-%d").build();
+        ThreadPoolTools threadPoolTools = new ThreadPoolTools();
+        long start = System.currentTimeMillis();
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        for (int i = 0; i < 20; i++) {
+            threadPoolTools.execute(() -> {
+                System.out.println("abcd");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                atomicInteger.addAndGet(1);
+            });
+        }
+        threadPoolTools.executor.shutdown();
+        while (!threadPoolTools.executor.awaitTermination(1, TimeUnit.SECONDS)) {
+            System.out.println("线程还在执行。。。");
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("一共处理了:"+ (end - start)+"ms");
+        if (atomicInteger.get() == 20) {
+            System.out.println("nice");
+        }
 
 
     }
