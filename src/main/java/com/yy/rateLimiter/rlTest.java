9127a75f20ac1b16da5a85a8cf1d2072ec2c3d51
YorickYu <521300259@qq.com>
2021-05-24 23:07:05 +0800

    20210524

---
+++ src/main/java/com/yy/rateLimiter/rlTest.java
@@ -0,0 +1,103 @@
+package com.yy.rateLimiter;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.util.concurrent.TimeUnit;
+
+public class rlTest {
+    public static void main(String[] args) {
+        testSmoothwarmingUp();
+    }
+
+    public static void testSmoothBursty() {
+        RateLimiter r = RateLimiter.create(5);
+        while (true) {
+            System.out.println("get 1 tokens: " + r.acquire() + "s");
+        }
+        /**
+         * output: Basically, the limiter is executed every 0.2s, which complies with the setting of releasing five tokens per second.
+         * get 1 tokens: 0.0s
+         * get 1 tokens: 0.182014s
+         * get 1 tokens: 0.188464s
+         * get 1 tokens: 0.198072s
+         * get 1 tokens: 0.196048s
+         * get 1 tokens: 0.197538s
+         * get 1 tokens: 0.196049s
+         */
+    }
+
+    public static void testSmoothBursty2() {
+        RateLimiter r = RateLimiter.create(2);
+        while (true)
+        {
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {}
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("end");
+            /**
+             * output:
+             * get 1 tokens: 0.0s
+             * get 1 tokens: 0.0s
+             * get 1 tokens: 0.0s
+             * get 1 tokens: 0.0s
+             * end
+             * get 1 tokens: 0.499796s
+             * get 1 tokens: 0.0s
+             * get 1 tokens: 0.0s
+             * get 1 tokens: 0.0s
+             */
+        }
+    }
+
+    public static void testSmoothBursty3() {
+        RateLimiter r = RateLimiter.create(5);
+        while (true)
+        {
+            System.out.println("get 5 tokens: " + r.acquire(5) + "s");
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("end");
+            /**
+             * output:
+             * get 5 tokens: 0.0s
+             * get 1 tokens: 0.996766s As a result of the delayed processing method, this thread waits for additional time on behalf of a previous thread.
+             * get 1 tokens: 0.194007s
+             * get 1 tokens: 0.196267s
+             * end
+             * get 5 tokens: 0.195756s
+             * get 1 tokens: 0.995625s As a result of the delayed processing method, this thread waits for additional time on behalf of a previous thread.
+             * get 1 tokens: 0.194603s
+             * get 1 tokens: 0.196866s
+             */
+        }
+    }
+
+    public static void testSmoothwarmingUp() {
+        RateLimiter r = RateLimiter.create(2, 3, TimeUnit.SECONDS);
+        while (true)
+        {
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("get 1 tokens: " + r.acquire(1) + "s");
+            System.out.println("end");
+            /**
+             * output:
+             * get 1 tokens: 0.0s
+             * get 1 tokens: 1.329289s
+             * get 1 tokens: 0.994375s
+             * get 1 tokens: 0.662888s  The total amount of time that has been taken for acquiring these three tokens is 3s.
+             * end
+             * get 1 tokens: 0.49764s  Tokens are acquired at the normal rate of two tokens/s.
+             * get 1 tokens: 0.497828s
+             * get 1 tokens: 0.49449s
+             * get 1 tokens: 0.497522s
+             */
+        }
+    }
+}
