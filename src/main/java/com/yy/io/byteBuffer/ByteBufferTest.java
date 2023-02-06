2a90ab44ed5609939d5895bf8cc202602bb50731
YorickYu <521300259@qq.com>
2023-02-06 23:33:00 +0800

    20230206

--- src/main/java/com/yy/io/byteBuffer/ByteBufferTest.java
+++ src/main/java/com/yy/io/byteBuffer/ByteBufferTest.java
@@ -5,6 +5,27 @@ import java.nio.charset.StandardCharsets;
 
 public class ByteBufferTest {
 
+    /**
+     * Invariants: mark <= position <= limit <= capacity
+     * private int mark = -1;
+     * private int position = 0;
+     * private int limit;
+     * private int capacity;
+     *
+     * byteBuffer.flip(); // 切换成读模式
+     * byteBuffer.clear(); // 切换成写模式，不保留未读数据
+     * byteBuffer.compact(); // 切换成写模式，保留未读数据
+     *
+     * byte b = byteBuffer.get(); // 读数据
+     * byte b1 = byteBuffer.get(1); // 读数据，不改变 position
+     *
+     * byteBuffer.rewind(); // position 回到 0
+     *
+     * byteBuffer.mark(); // mark 当前 position
+     * byteBuffer.reset(); // position 回到 mark 的位置
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         String str = "hello world!";
